85526990c73fc485a8cb8c3b3ea788bd50f60ba3
denanrioaditya <waldedan93@egmail.com>
2022-07-13 12:39:02 +0700

    first_commit

---
+++ phase_0/week_1/main.py
@@ -0,0 +1,15 @@
+#from pack1.module1 import *
+
+#print (kurang(10,5))
+
+#print (tambah(10,5))
+
+#from pack1.module2 import cetak_nama
+
+#cetak_nama('rachman','instruktur')
+
+#from pack1 import module1 as m
+
+from pack1 import cetak_nama
+
+cetak_nama()
---
+++ phase_0/week_1/pack1/module2.py
@@ -0,0 +1,4 @@
+def cetak_nama(var1,var2):
+  print(f'nama saya adalah {var1} dan saya adalah {var2}')
+
+cetak_nama('rachman','instruktur') 
